0b148b5f19e574e545d079ea56104ff7fd7f43d9
techgoaty <153655883+techgoaty@users.noreply.github.com>
2024-03-06 08:27:18 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -20,8 +20,8 @@ global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
 global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
+global.ownernumber = '2349169580302' //ur owner number
+global.ownername = 'ANONYMOUS🎭' //ur owner name
 global.websitex = "https://youtu.be/mu5m6aB6P5k"
 global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
 global.themeemoji = '🪀'
@@ -31,7 +31,7 @@ global.packname = "Sticker By"
 global.author = "🦄드림 가이 Xeon\n\n+916909137213"
 global.creator = "916909137213@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.premium = ["2348118820641"] // Premium User
 global.hituet = 0
 
 //bot sett
